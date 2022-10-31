aafe78f5f56d3ab8f426294844ff987666ec8fe5
sabbir <sabbir@gmail.com>
2022-10-31 21:17:13 +0600

    calculator style

--- src/App.js
+++ src/App.js
@@ -15,24 +15,25 @@ function App() {
   };
 
   return (
-    <div className='calculator'>
-      <div className="display">
-        <span>(0)</span>0
+    <div className="App">
+      <div className='calculator'>
+        <div className="display">
+          <span>(0)</span>0
+        </div>
+        <div className="operators">
+          <button>/</button>
+          <button>*</button>
+          <button>+</button>
+          <button>-</button>
+          <button>DEL</button>
+        </div>
+        <div className="digits">
+          {createDigits()}
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
       </div>
-      <div className="oparetors">
-        <button>/</button>
-        <button>*</button>
-        <button>+</button>
-        <button>-</button>
-        <button>DEL</button>
-      </div>
-      <div className="digits">
-        {createDigits()}
-        <button>0</button>
-        <button>.</button>
-        <button>=</button>
-      </div>
-
     </div>
   );
 }
